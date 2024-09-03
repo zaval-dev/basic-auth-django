7adf47f9ed005cbd0a9f3a795e83ea10c2d6b3ed
zaval-dev <danielzavaletamego@gmail.com>
2024-09-03 17:37:44 -0500

    CONFIGURE login and login success path

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -132,3 +132,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/dashboard/'
